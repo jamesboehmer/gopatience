240e64c21346f638c4e77b9b37c6e9e8381e99b7
James Boehmer <james.boehmer@gmail.com>
2020-05-24 11:47:54 -0400

    GH-1: Brought cards coverage to 100%

--- pkg/cards/cards_test.go
+++ pkg/cards/cards_test.go
@@ -2,12 +2,13 @@ package cards
 
 import (
 	"github.com/jamesboehmer/gopatience/pkg/cards/pip"
+	"github.com/jamesboehmer/gopatience/pkg/cards/suit"
 	"strings"
 	"testing"
 )
 
 func TestCardFaces(t *testing.T) {
-	deck := NewDeck(1, 0)
+	deck := NewDeck(1, 2)
 	facePips := map[pip.Pip]interface{}{pip.Jack: nil, pip.Queen: nil, pip.King: nil}
 	for _, card := range deck.Cards {
 		_, found := facePips[card.Pip]
@@ -68,7 +69,7 @@ func TestDeck_Shuffle(t *testing.T) {
 
 func TestDeck_Deal_Remaining(t *testing.T) {
 	deck := NewDeck(1, 0)
-	if deck.Remaining() != 52{
+	if deck.Remaining() != 52 {
 		t.Error("New Deck should have 52 cards.")
 	}
 	card1 := deck.Cards[0]
@@ -83,22 +84,32 @@ func TestDeck_Deal_Remaining(t *testing.T) {
 	if deck.Remaining() != 49 {
 		t.Error("Deck should have 49 cards remaining.")
 	}
+	for i := 0; i < 49; i++ {
+		deck.Deal()
+	}
+	if deck.Remaining() != 0 {
+		t.Error("Deck should have 0 cards remaining.")
+	}
+	card := deck.Deal()
+	if card != nil {
+		t.Error("Deck should deal nil when empty")
+	}
 
 }
 
 func TestCard_Reveal_Conceal(t *testing.T) {
 	card := new(Card).Reveal()
-	if !card.Revealed{
+	if !card.Revealed {
 		t.Error("Card should be revealed")
 	}
 	card.Conceal()
-	if card.Revealed{
+	if card.Revealed {
 		t.Error("Card should be concealed")
 	}
 }
 
 func TestCard_String(t *testing.T) {
-	deck := NewDeck(1, 0)
+	deck := NewDeck(1, 1)
 	for deck.Remaining() > 0 {
 		card := deck.Deal()
 		cardString := []rune(card.String())
@@ -109,15 +120,76 @@ func TestCard_String(t *testing.T) {
 		if string(cardString[0]) == "|" {
 			t.Error("Revealed card string should not start with |")
 		}
-		suitString := cardString[len(cardString)-1:]
-		if string(suitString) != string(card.Suit) {
-			t.Error("Card string suit should match the card's Suit")
-		}
-		pipString := cardString[:len(cardString)-1]
-		if string(pipString) != string(card.Pip) {
-			t.Error("Card string pip should match the card's Pip")
+		if card.Suit == "" {
+			if string(cardString) != "*" {
+				t.Error("Joker string should be an asterisk")
+			}
+		} else {
+			suitString := cardString[len(cardString)-1:]
+			if string(suitString) != string(card.Suit) {
+				t.Error("Card string suit should match the card's Suit")
+			}
+			pipString := cardString[:len(cardString)-1]
+			if string(pipString) != string(card.Pip) {
+				t.Error("Card string pip should match the card's Pip")
+			}
 		}
 
 	}
 }
 
+func TestParseCard(t *testing.T) {
+	_, err := ParseCard("|10x")
+	if err == nil {
+		t.Error("Bad suit string should have returned an error")
+	}
+	_, err = ParseCard("Z♠")
+	if err == nil {
+		t.Error("Bad pip string should have returned an error")
+	}
+	card, err := ParseCard("*")
+	if err != nil {
+		t.Error("Joker card should have been parsed")
+	}
+	if card.Pip != "" || card.Suit != "" {
+		t.Error("Joker suit and pip should be empty")
+	}
+	for _, suit := range suit.Suits {
+		for _, pip := range pip.Pips {
+			for _, revealed := range []bool{true, false} {
+				card := Card{pip, suit, revealed}
+				cardString := card.String()
+				parsedCard, err := ParseCard(cardString)
+				if err != nil {
+					t.Error("Card parse should not have failed.")
+				} else {
+					if parsedCard.Pip != pip {
+						t.Error("Parsed card pip doesn't match.")
+					}
+					if parsedCard.Suit != suit {
+						t.Error("Parsed card suit doesn't match.")
+					}
+					if parsedCard.Revealed != revealed {
+						t.Error("Parsed card state doesn't match.")
+					}
+				}
+
+			}
+		}
+	}
+}
+
+func TestSuitColor(t *testing.T) {
+	if suit.Spades.Color() != suit.Black {
+		t.Errorf("%s should be black", suit.Spades)
+	}
+	if suit.Clubs.Color() != suit.Black {
+		t.Errorf("%s should be black", suit.Clubs)
+	}
+	if suit.Diamonds.Color() != suit.Red {
+		t.Errorf("%s should be red", suit.Diamonds)
+	}
+	if suit.Hearts.Color() != suit.Red {
+		t.Errorf("%s should be red", suit.Hearts)
+	}
+}
